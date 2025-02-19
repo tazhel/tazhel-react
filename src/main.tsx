84e75dd4a27e7124a245510aa20dd271b64eaf66
tazhel <tazhel@gmail.com>
2025-02-19 14:22:44 +0100

    removed .tsx in import

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Footer from './components/footer/Footer.tsx';
-import Header from './components/header/Header.tsx';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
 import './index.css';
-import AboutPage from './pages/about-page/AboutPage.tsx';
-import GamesPage from './pages/games-page/GamesPage.tsx';
-import HomePage from './pages/home-page/HomePage.tsx';
-import ModsPage from './pages/mods-page/ModsPage.tsx';
-import OtherPage from './pages/other-page/OtherPage.tsx';
+import AboutPage from './pages/about-page/AboutPage';
+import GamesPage from './pages/games-page/GamesPage';
+import HomePage from './pages/home-page/HomePage';
+import ModsPage from './pages/mods-page/ModsPage';
+import OtherPage from './pages/other-page/OtherPage';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
