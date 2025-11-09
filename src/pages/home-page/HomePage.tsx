7a92252605f54d2eee6d06fd98e626a4ca64f1c3
tazhel <tazhel@gmail.com>
2025-11-09 22:42:14 +0100

    trying to fix 153 youtube error

--- src/pages/home-page/HomePage.tsx
+++ src/pages/home-page/HomePage.tsx
@@ -2,15 +2,21 @@ import '../../index.css';
 import './homePage.css';
 
 const HomePage: React.FC = () => {
+    const youtubeVideoId = 'HagSECM08yE';
+    const origin = encodeURIComponent('https://www.tazhel.com');
+
+    const embedUrl = `https://www.youtube-nocookie.com/embed/${youtubeVideoId}?autoplay=1&mute=1&rel=0&controls=0&modestbranding=1&fs=0&vq=hd1080&origin=${origin}`;
+
     return (
         <div className="page-container">
             <iframe
                 className="page-youtube"
-                src="https://www.youtube.com/embed/HagSECM08yE?autoplay=1&mute=1&rel=0&controls=0&modestbranding=1&fs=0&vq=hd1080"
+                src={embedUrl}
                 title="YouTube video player"
                 style={{ border: 'none' }}
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
+                referrerPolicy="strict-origin-when-cross-origin"
             ></iframe>
         </div>
     );
